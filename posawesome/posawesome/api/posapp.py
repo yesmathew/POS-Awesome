413117a92120d540eb08b593771ca044b3be2ea8
Nihalroshan55 <nihalroshan55@gmail.com>
2024-11-14 18:06:40 +0530

    add pos opening shift from sales invoice

--- posawesome/posawesome/api/posapp.py
+++ posawesome/posawesome/api/posapp.py
@@ -1745,3 +1745,34 @@ def get_seearch_items_conditions(item_code, serial_no, batch_no, barcode):
     return """ and (name like {item_code} or item_name like {item_code})""".format(
         item_code=frappe.db.escape("%" + item_code + "%")
     )
+
+
+
+@frappe.whitelist()
+def get_openingshift(doc,method):
+    user=frappe.session.user
+    open_vouchers = frappe.db.get_all(
+        "POS Opening Shift",
+        filters={
+            "user": user,
+            "pos_closing_shift": ["in", ["", None]],
+            "docstatus": 1,
+            "status": "Open",
+        },
+        fields=["name", "pos_profile"],
+        order_by="period_start_date desc"
+    )
+    if  doc.pos_profile:
+        if open_vouchers and  open_vouchers[0].name  :
+            if doc.pos_profile == open_vouchers[0].pos_profile:
+                if method=='validate':
+                    doc.posa_pos_opening_shift=open_vouchers[0].name
+                else:
+                    sales_invoice = frappe.get_doc('Sales Invoice', doc.name)
+                    sales_invoice.posa_pos_opening_shift = open_vouchers[0].name
+                    sales_invoice.save(ignore_permissions=True)  
+            else:
+                frappe.throw("The Openig shift pos in Pos and your is not matching")
+        else:
+            frappe.throw("Create an Open Shift in Pos")
+    return 

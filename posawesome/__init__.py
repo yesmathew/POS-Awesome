901243a35d824fd4851a8424847e033ba1050cc7
RahulK2202 <rahuljcet95@gmail.com>
2025-11-10 11:18:15 +0530

    feat(init): lazy import frappe to fix install-time import error

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import frappe
+
 
 __version__ = "6.1.3"
 
 
 def console(*data):
+    import frappe
     frappe.publish_realtime("toconsole", data, user=frappe.session.user)
